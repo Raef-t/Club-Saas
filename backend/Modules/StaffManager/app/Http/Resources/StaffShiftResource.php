d19dd5967f33dea5d256c9e0b13f66831033b09c
Yaman Abdullah <yamanabdullah370@gmail.com>
2026-07-09 11:59:22 +0300

    fic error

--- backend/Modules/StaffManager/app/Http/Resources/StaffShiftResource.php
+++ backend/Modules/StaffManager/app/Http/Resources/StaffShiftResource.php
@@ -1,11 +1,19 @@
 <?php
+
 namespace Modules\StaffManager\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class StaffShiftResource extends JsonResource
 {
-    public function toArray($request) {
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
         return [
             'id' => $this->id,
             'staff_id' => $this->staff_id,
