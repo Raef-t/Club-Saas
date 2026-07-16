fe8e0520baddf538038ef58355c496025b64f938
Yaman Abdullah <yamanabdullah370@gmail.com>
2026-07-16 13:45:00 +0300

    solve proplem in link plan witrh coah

--- backend/Modules/SubscriptionManager/app/Services/SubscriptionPlanActivityService.php
+++ backend/Modules/SubscriptionManager/app/Services/SubscriptionPlanActivityService.php
@@ -20,7 +20,33 @@ class SubscriptionPlanActivityService
         return $this->repository->all(); 
     }
     public function getById($id) { return $this->repository->find($id); }
-    public function create(array $data) { return $this->repository->create($data); }
-    public function update($id, array $data) { return $this->repository->update($id, $data); }
+    public function create(array $data) { 
+        if (isset($data['activity_id'])) {
+            $staffActivity = \Modules\Sports\Models\StaffActivity::firstOrCreate([
+                'activity_id' => $data['activity_id'],
+                'staff_id' => $data['coach_id'] ?? null,
+            ]);
+            $data['staff_activity_id'] = $staffActivity->id;
+            unset($data['activity_id'], $data['coach_id']);
+        }
+        return $this->repository->create($data); 
+    }
+
+    public function update($id, array $data) { 
+        if (array_key_exists('activity_id', $data) || array_key_exists('coach_id', $data)) {
+            $current = $this->getById($id);
+            $activityId = $data['activity_id'] ?? $current->activity_id;
+            $coachId = array_key_exists('coach_id', $data) ? $data['coach_id'] : $current->coach_id;
+
+            $staffActivity = \Modules\Sports\Models\StaffActivity::firstOrCreate([
+                'activity_id' => $activityId,
+                'staff_id' => $coachId,
+            ]);
+            $data['staff_activity_id'] = $staffActivity->id;
+            unset($data['activity_id'], $data['coach_id']);
+        }
+        return $this->repository->update($id, $data); 
+    }
+
     public function delete($id) { return $this->repository->delete($id); }
 }
